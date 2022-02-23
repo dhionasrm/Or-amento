76ad27adca5426c99f6674162afa9257ec161c4e
dhionas magalhães <dhionas17@hotmail.com>
2022-02-23 15:50:27 -0300

    Ajustes de impressão

--- Orçamento/Form1.cs
+++ Orçamento/Form1.cs
@@ -15,54 +15,50 @@ namespace Orçamento
         public Form1()
         {
             InitializeComponent();
+
         }
+          
+       
 
         private void imprimirButton_Click(object sender, EventArgs e)
         {
-            //printDocument.Print();
-            using (var pd = new System.Drawing.Printing.PrintDocument())
-            {
-                //pd.PrinterSettings.PrinterName = impressoraComboBox.SelectedItem.ToString();
-                pd.PrintPage += printDocument_PrintPage;
-                pd.Print();
-            }
-        }
 
-
-        private void printDocument_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
-        {
-            using (var font = new Font("Times New Roman", 14))
-            using (var brush = new SolidBrush(Color.Black))
+            
+            using (var printDocument = new System.Drawing.Printing.PrintDocument())
             {
-                e.Graphics.DrawString(TituloEmpresa.Text, font, brush, e.MarginBounds);
+                
+
+                printDocument.PrintPage += printDocument_PrintPage;
+                //printDocument.PrinterSettings.PrinterName = impressoraComboBox.SelectedItem.ToString();
+                printDocument.Print();
+
                 
             }
-
-            using (var font = new Font("Times New Roman", 14))
-            using (var brush = new SolidBrush(Color.Black))
-            {
-                e.Graphics.DrawString(TituloEmpresa.Text, font, brush, e.MarginBounds);
-                e.Graphics.DrawString(TituloOrcamento.Text, font, brush, e.MarginBounds);
-                e.Graphics.DrawString(NomeEmpresa.Text, font, brush, e.MarginBounds);
-                e.Graphics.DrawString(CNPJ_Empresa.Text, font, brush, e.MarginBounds);
-                e.Graphics.DrawString(lblData.Text, font, brush, e.MarginBounds);
-                e.Graphics.DrawString(dateTimePicker1.Text, font, brush, e.MarginBounds);
-                e.Graphics.DrawString(lblCliente.Text, font, brush, e.MarginBounds);
-                e.Graphics.DrawString(textBox1.Text, font, brush, e.MarginBounds);
-                e.Graphics.DrawString(textBox2.Text, font, brush, e.MarginBounds);
-                e.Graphics.DrawString(textBox3.Text, font, brush, e.MarginBounds);
-                e.Graphics.DrawString(lblDesc.Text, font, brush, e.MarginBounds);
-                e.Graphics.DrawString(label9.Text, font, brush, e.MarginBounds);
-                e.Graphics.DrawString(label10.Text, font, brush, e.MarginBounds);
-
-            }
-
-            /*var image = new Bitmap(this.Width, this.Height);
-            var graphics = Graphics.FromImage(image);
-            graphics.CopyFromScreen(this.Location.X, this.Location.Y, 0, 0, this.Size);
-            e.Graphics.DrawImage(image, 20, 20);*/   
         }
 
-        
+        void printDocument_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            String Dados;
+
+            Dados = $"{TituloEmpresa.Text}\n{TituloOrcamento.Text}\n{NomeEmpresa.Text }\n{CNPJ_Empresa.Text}\n{textBox1.Text}";
+
+            var printDocument = sender as System.Drawing.Printing.PrintDocument;
+
+            if (printDocument != null)
+            {
+                using (var font = new Font("Times New Roman", 14))
+                using (var brush = new SolidBrush(Color.Black))
+                {
+                    e.Graphics.DrawString(
+                        Dados,
+                font,
+                brush,
+                new RectangleF(0, 0, printDocument.DefaultPageSettings.PrintableArea.Width, printDocument.DefaultPageSettings.PrintableArea.Height));
+                }
+            }
+        }
     }
 }
+
+
+        
